20ce929950e5620f5e42185f0c45dd3bf14a8ff1
Michael Powell <powell.r.mike@gmail.com>
2013-07-05 16:11:59 -0700

    Initial commit

---
+++ src/fireminder/firepedometer/FirePedometerActivity.java
@@ -0,0 +1,15 @@
+package fireminder.firepedometer;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FirePedometerActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
